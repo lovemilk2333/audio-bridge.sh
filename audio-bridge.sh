03adec91faa3570a43d129dff1d07f9c6e66e970
lovemilk2333 <zhuhansan666@outlook.com>
2025-04-20 20:53:26 +0800

    add: audio bridge custom name support

--- audio-bridge.sh
+++ audio-bridge.sh
@@ -2,10 +2,14 @@
 # This is free and unencumbered software released into the public domain.
 
 cmd_name="audio-bridge.sh"
-version="v1"
+version="v0.1.0"
 
 function CreateSource {
-    local dev_name="audio-bridge"
+    if [ -z "$1" ]; then
+        local dev_name="audio-bridge"
+    else
+        local dev_name="$1"
+    fi
 
     pactl load-module module-null-sink \
         media.class=Audio/Source/Virtual sink_name="$dev_name" \
@@ -14,7 +18,7 @@ function CreateSource {
 
 case "$1" in
 create)
-    CreateSource || exit $?
+    CreateSource $2 || exit $?
     echo "use this command to delete devices:"
     echo "pactl unload-module <ID>"
     ;;
@@ -24,7 +28,7 @@ version | --version | -v)
 help | --help | -h | *)
     echo "Usage: $cmd_name <Action>"
     echo "Action:"
-    echo -e "    create\t\tCreate a bridge devices via pactl"
+    echo -e "    create [dev_name]\tCreate a bridge devices via pactl (default name is \`audio-bridge\`)"
     echo -e "    version\t\tShow script version, compatible --version and -v"
     echo -e "    help(default)\tShow this help message, compatible --help and -h"
     ;;
